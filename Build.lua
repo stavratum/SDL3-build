c4a5f88f61d52b29ed8a6e989876c5cf6c6a4f87
stavratum <89344034+stavratum@users.noreply.github.com>
2025-05-23 02:07:40 +0500

    Update Build.lua

--- Build.lua
+++ Build.lua
@@ -1,17 +1,12 @@
--- premake5.lua
-workspace "New Project"
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+workspace "SDL Build Test"
+   startproject "SDL"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "App"
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
       buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
-OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
-
-group "Core"
-	include "Core/Build-Core.lua"
-group ""
-
-include "App/Build-App.lua"
+include "Build-SDL.lua"
